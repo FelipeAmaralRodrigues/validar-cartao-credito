a2b5afe8ab81577d78a8743726fc08d21235ae50
Felipe <felipeamaralrodrigues@gmail.com>
2018-03-28 17:37:16 -0300

    primeiro commit

---
+++ leanwork/src/LeanWork.Domain/CartaoCredito/CartaoCredito.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;               
+using LeanWork.Domain.Core.Models;
+
+namespace LeanWork.Domain.CartaoCredito
+{
+    public class CartaoCredito : Entity<CartaoCredito>
+    {
+        public string Numero { get; private set; }
+
+        public string Bandeira { get; private set; }     
+        
+        public override bool EhValido()
+        {
+            // Tome uma sequência de números inteiros positivos e a inverta. 
+            string[] arrayNumero = new string[Numero.Length];
+            for (var i = 0; i < Numero.Length; i++)
+            {
+                arrayNumero[i] = Numero[i].ToString();
+            }                       
+            Array.Reverse(arrayNumero);
+
+            //Começando pelo segundo número, dobre o valor de cada número de forma alternada ("24145... = "28185...).
+            int numero = -1;
+            for(var i = 1; i < arrayNumero.Length; i++)
+            {            
+                if (!int.TryParse(arrayNumero[i], out numero))
+                {
+                    return false;
+                }
+
+                if (i % 2 != 0)
+                {
+                    arrayNumero[i] = (numero * 2).ToString();
+                }
+            }
+
+            // Para dígitos maiores que 9 será necessário que some cada dígito ("10", 1 + 0 = 1) ou subtraia por 9 ("10", 10 - 9 = 1).
+            for (var i = 1; i < arrayNumero.Length; i++)
+            {
+                if (arrayNumero[i].Length == 2)
+                {            
+                    arrayNumero[i] = (int.Parse(arrayNumero[i][0].ToString()) + int.Parse(arrayNumero[i][1].ToString())).ToString();
+                }
+            }
+
+            // Some todos os números da sequência.
+            var soma = arrayNumero.Sum(a => int.Parse(a));
+
+            // Se o total for múltiplo de 10, o número é válido.
+            if (soma % 10 == 0)
+                return true;
+            return false;   
+        }
+
+        public static class CartaoCreditoFactory
+        {
+            public static CartaoCredito NovoCartaoCredito(string numero)
+            {
+                var cartaoCredito = new CartaoCredito()
+                {
+                    Numero = numero
+                };
+
+                // AMEX
+                if ((numero.Trim().StartsWith("34") || numero.Trim().StartsWith("37")) && numero.Trim().Length == 15)
+                    cartaoCredito.Bandeira = "AMEX";
+
+                // Discover
+                else if (numero.Trim().StartsWith("6011") && numero.Trim().Length == 16)
+                    cartaoCredito.Bandeira = "Discover";
+
+                // MasterCard
+                else if ((numero.Trim().StartsWith("51") || numero.Trim().StartsWith("55")) && numero.Trim().Length == 16)
+                    cartaoCredito.Bandeira = "MasterCard";
+
+                // Visa
+                else if (numero.Trim().StartsWith("4") && (numero.Trim().Length == 13 || numero.Trim().Length == 16))
+                    cartaoCredito.Bandeira = "Visa";
+
+                // Outra
+                else cartaoCredito.Bandeira = "Desconhecido";
+                                                   
+                return cartaoCredito;
+            }
+        }
+
+    }
+}
---
+++ leanwork/tests/LeanWork.Tests/Unit Tests/CartaoCreditoTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeanWork.Domain.CartaoCredito;
+using Xunit;
+
+namespace LeanWork.Tests.Unit_Tests
+{
+    public class CartaoCreditoTests
+    {
+        // AAA => Arrange, Act, Assert
+
+        [Fact]
+        [Trait("CartaoCredito", "Visa")]
+        public void CartaoCredito_ValidarVisa_RetornarComSucesso() 
+        {
+            // Arrange                            
+            CartaoCredito c = CartaoCredito.CartaoCreditoFactory.NovoCartaoCredito("4111111111111111");
+
+            // Act
+            var result = c.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.StrictEqual(c.Bandeira,"Visa");
+        }
+
+        [Fact]
+        [Trait("CartaoCredito", "Visa")]
+        public void CartaoCredito_ValidarVisa_RetornarComFalha()
+        {
+            // Arrange                            
+            CartaoCredito c = CartaoCredito.CartaoCreditoFactory.NovoCartaoCredito("4111111111111");
+
+            // Act
+            var result = c.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.StrictEqual(c.Bandeira,"Visa");
+        }
+
+        [Fact]
+        [Trait("CartaoCredito", "Amex")]
+        public void CartaoCredito_ValidarAmex_RetornarComSucesso()
+        {
+            // Arrange                            
+            CartaoCredito c = CartaoCredito.CartaoCreditoFactory.NovoCartaoCredito("378282246310005");
+
+            // Act
+            var result = c.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.StrictEqual(c.Bandeira,"AMEX");
+        }
+
+        [Fact]
+        [Trait("CartaoCredito", "MasterCard")]
+        public void CartaoCredito_ValidarMasteCard_RetornarComSucesso()
+        {
+            // Arrange                            
+            CartaoCredito c = CartaoCredito.CartaoCreditoFactory.NovoCartaoCredito("5105105105105100");
+
+            // Act
+            var result = c.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.StrictEqual(c.Bandeira,"MasterCard");
+        }
+
+        [Fact]
+        [Trait("CartaoCredito", "MasterCard")]
+        public void CartaoCredito_ValidarMasteCard_RetornarComFalha()
+        {
+            // Arrange                            
+            CartaoCredito c = CartaoCredito.CartaoCreditoFactory.NovoCartaoCredito("5105105105105106");
+
+            // Act
+            var result = c.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.StrictEqual(c.Bandeira,"MasterCard");
+        }
+
+        [Fact]
+        [Trait("CartaoCredito", "Discover")]
+        public void CartaoCredito_ValidarDiscover_RetornarComSucesso()
+        {
+            // Arrange                            
+            CartaoCredito c = CartaoCredito.CartaoCreditoFactory.NovoCartaoCredito("6011111111111117");
+
+            // Act
+            var result = c.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.StrictEqual(c.Bandeira,"Discover");
+        }
+
+        [Fact]
+        [Trait("CartaoCredito", "Desconhecido")]
+        public void CartaoCredito_ValidarDesconhecido_RetornarComFalha()
+        {
+            // Arrange                            
+            CartaoCredito c = CartaoCredito.CartaoCreditoFactory.NovoCartaoCredito("9111111111111111");
+
+            // Act
+            var result = c.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.StrictEqual(c.Bandeira,"Desconhecido");
+        }
+    }
+}
